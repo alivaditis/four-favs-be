6919d3dea21af998afd3e37db567c2f33ab73dfe
Alec Livaditis <aleclivaditis@gmail.com>
2023-11-16 19:24:35 -0500

    add error handling for route

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
+import { handleInputErrors } from './modules/middleware';
 import { updateFavs } from './handlers/user';
 
 const router = Router()
@@ -8,6 +9,11 @@ router.put('/user/:username/favs', [
   body('fourFavs')
     .isArray().withMessage('favs must be an array')
     .custom(value => value.every(item => typeof item === 'number')).withMessage('All elements in favs must be numbers'),
-], updateFavs)
+], handleInputErrors, updateFavs)
+
+router.use((err, req, res, next) => {
+  console.log(err)
+  res.json({message:'in router handler'})
+})
 
 export default router
